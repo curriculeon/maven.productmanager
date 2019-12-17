1a0df64a620a18b0efc63cc6840042feac2c664a
santa-js-html <sanafaris2000@yahoo.com>
2019-12-17 12:08:48 -0500

    Done 12-17

--- src/main/java/com/github/curriculeon/ItemInventory.java
+++ src/main/java/com/github/curriculeon/ItemInventory.java
@@ -1,31 +1,40 @@
 package com.github.curriculeon;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by leon on 12/16/2019.
  */
-public class ItemInventory implements InventoryInterface<Item> {
+public class ItemInventory implements InventoryInterface<Item>, Iterable<Item> {
+    List<Item> productManager = new ArrayList<>();
     @Override
     public void add(Item someObject) {
-
+        this.productManager.add(someObject);
     }
 
     @Override
     public Boolean contains(Item someObject) {
-        return null;
+        return this.productManager.contains(someObject);
     }
 
     @Override
     public Item get(int indexOfElement) {
-        return null;
+        return this.productManager.get(indexOfElement);
     }
 
     @Override
     public Item remove(int indexOfElement) {
-        return null;
+        return this.productManager.remove(indexOfElement);
     }
 
     @Override
     public Item[] toArray(Item[] objectsToBeAdded) {
-        return new Item[0];
+        return this.productManager.toArray(objectsToBeAdded);
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return this.productManager.iterator();
     }
 }
